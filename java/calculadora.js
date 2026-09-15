8a6a152156b55169d8e381d2f5e860729576b09e
raildon <raildonprogramador@gmail.com>
2026-09-15 09:06:59 -0300

    atualizar repositorio

---
+++ java/calculadora.js
@@ -0,0 +1,17 @@
+function adicionar(valor) {
+    document.getElementById("visor").value += valor;
+}
+
+function limpar() {
+    document.getElementById("visor").value = "";
+}
+
+function calcular() {
+    let expressao = document.getElementById("visor").value;
+
+    try {
+        document.getElementById("visor").value = eval(expressao);
+    } catch {
+        document.getElementById("visor").value = "Erro";
+    }
+}
